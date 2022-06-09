596ab5d686b08561008c3881cf63d3896f080490
vSsongv <songdh21@naver.com>
2022-06-10 03:38:58 +0900

    ✨conf: recoil set

--- App.tsx
+++ App.tsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
 import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -6,11 +13,13 @@ import MainNavigation from './src/screens/MainNavigation';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <MainNavigation />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <RecoilRoot>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <MainNavigation />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </RecoilRoot>
   );
 }
 

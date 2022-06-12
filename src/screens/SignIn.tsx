6e2d0b8da3e59ca17f919ca780719a42436fd43f
vSsongv <songdh21@naver.com>
2022-06-13 04:55:10 +0900

    ✨feat: got token

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,29 +1,46 @@
 import axios from 'axios';
-import {StyleSheet, TextInput, View} from 'react-native';
+import {StyleSheet, TextInput, TouchableOpacity, Text} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AsyncStorage from '@react-native-community/async-storage';
+import {useForm} from 'react-hook-form';
 import React, {useState} from 'react';
 
 const SignIn = () => {
-  axios.post('http://???/api/v1/auth/login');
+  const {register, handleSubmit, getValues} = useForm();
 
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
+  const URL = 'http://15.165.27.120:8080';
+
+  const [id, setId] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const doSignIn = async () => {
+    try {
+      const data = await axios.post(URL + '/api/v1/auth/login', {
+        user_id: id,
+        password: password,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <SafeAreaProvider>
       <TextInput
-        placeholder="ID"
+        placeholder="아이디"
         value={id}
         onChangeText={setId}
         style={styles.textInput}
       />
       <TextInput
-        placeholder="Password"
+        placeholder="비밀번호"
         value={password}
         onChangeText={setPassword}
         style={styles.textInput}
       />
+      <TouchableOpacity style={styles.loginBtn} onPress={doSignIn}>
+        <Text style={{color: '#ffffff'}}>로그인</Text>
+      </TouchableOpacity>
     </SafeAreaProvider>
   );
 };
@@ -40,5 +57,14 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginBottom: 20,
     paddingLeft: 10,
+  },
+  loginBtn: {
+    marginTop: 20,
+    width: 270,
+    height: 42,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FF3196',
   },
 });

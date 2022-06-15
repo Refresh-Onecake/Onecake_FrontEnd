96e320d7fc1178bb56d184cdf83465076b7139d9
vSsongv <songdh21@naver.com>
2022-06-16 03:59:17 +0900

    📝 add 2 new screen file - 비밀번호 찾기 - 사용자, 판매자

---
+++ src/screens/FindPwd.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const FindPwd = () => {
+  return (
+    <View>
+      <Text>FindPwd</Text>
+    </View>
+  );
+};
+
+export default FindPwd;
+
+const styles = StyleSheet.create({});

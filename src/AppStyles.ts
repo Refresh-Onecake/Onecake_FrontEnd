ef68b7be6233b347221e48ee545bed1ad55f46c8
min <hororonoodle@gmail.com>
2022-06-15 23:06:09 +0900

    feat : 글로벌 스타일 추가

---
+++ src/AppStyles.ts
@@ -0,0 +1,17 @@
+export const AppStyles = {
+  color: {
+    black: '#000000',
+    gray: '#707070',
+    border: '#E7E7E7',
+    white: '#FFFFFF',
+    pink: '#FF4EA5',
+    hotPink: '#FF3196',
+  },
+  padding: {
+    screen: 32.06,
+  },
+  IconSize: {
+    inputIcon: 15,
+    checkIcon: 18,
+  },
+};

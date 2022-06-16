0d7d1098d3fb1eaf8e53cb3b5d8c6681d266ea4d
vSsongv <songdh21@naver.com>
2022-06-17 04:39:18 +0900

    ✨navigation done

--- src/screens/SelectUserType.tsx
+++ src/screens/SelectUserType.tsx
@@ -1,14 +1,30 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import React from 'react';
 
 const SelectUserType = ({navigation}) => {
+  const goToSignUp = () => {
+    navigation.navigate('SignUp');
+  };
   return (
-    <View>
-      <Text>SelectionUserType</Text>
-    </View>
+    <SafeAreaProvider>
+      <TouchableOpacity onPress={goToSignUp} style={styles.typeBtn}>
+        <Text>소비자</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={goToSignUp} style={styles.typeBtn}>
+        <Text>판매자</Text>
+      </TouchableOpacity>
+    </SafeAreaProvider>
   );
 };
 
 export default SelectUserType;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  typeBtn: {
+    width: 178,
+    height: 186,
+    backgroundColor: 'yellow',
+    marginBottom: 21,
+  },
+});

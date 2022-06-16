859c88528fd2550edca9ea9347b2234a6456676d
min <hororonoodle@gmail.com>
2022-06-16 19:02:40 +0900

    chore : setting react-query

--- App.tsx
+++ App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil';
+//prettier-ignore
+import {RecoilRoot, atom, selector, useRecoilState, useRecoilValue} from 'recoil';
+//prettier-ignore
+import {useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider} from 'react-query';
 import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import MainNavigation from './src/screens/MainNavigation';
 
+//Create a client
+const queryClient = new QueryClient();
 export default function App() {
   return (
-    <RecoilRoot>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <MainNavigation />
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <MainNavigation />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 

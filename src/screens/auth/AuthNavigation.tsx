b88f1601c5296f740a4ffa88db55e4a22e179d81
vSsongv <songdh21@naver.com>
2022-06-17 19:07:47 +0900

    ✨feat: userType 전달

--- src/screens/auth/AuthNavigation.tsx
+++ src/screens/auth/AuthNavigation.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import SignUp from './SignUp';
-import SignIn from './SignIn';
-import SelectUserType from './SelectUserType';
-import FindPwd from './FindPwd';
+import SignUp from '../auth/SignUp';
+import SignIn from '../auth/SignIn';
+import SelectUserType from '../auth/SelectUserType';
+import FindPwd from '../auth/FindPwd';
 import MainNavigation from '../MainNavigation';
 
 const Stack = createStackNavigator();
 
-const AuthNavigation = ({navigation}: {navigation: any}) => {
+const AuthNavigation = ({navigation}: {navigation: any}, userType: string) => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="SignIn" component={SignIn} navigation={navigation} />
-      <Stack.Screen name="SignUp" component={SignUp} navigation={navigation} />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        navigation={navigation}
+        userType={userType}
+      />
       <Stack.Screen
         name="SelectUserType"
         component={SelectUserType}

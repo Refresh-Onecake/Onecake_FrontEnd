b163eee58401c817f3723b6b9613adb19fb5dbf2
vSsongv <songdh21@naver.com>
2022-06-26 04:14:24 +0900

    ✨feat: store tab에서 store detail페이지로 이동하도록 설정

--- src/screens/Stores.tsx
+++ src/screens/Stores.tsx
@@ -1,11 +1,26 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {FC} from 'react';
 import {AppStyles} from '../styles/AppStyles';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {StackScreenProps} from '@react-navigation/stack';
+import {RootStackParamList} from '../screens/navigator';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-const Stores = () => {
+const Stores = ({navigation}: StackScreenProps<RootStackParamList>) => {
   return (
     <SafeAreaView style={styles.view}>
-      <Text>Store</Text>
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => navigation.navigate('StoreDetail')}>
+        <Icon
+          style={{width: 18, height: 18, position: 'absolute', right: 0}}
+          name="home-heart"
+        />
+        <Image
+          style={styles.image}
+          source={require('../asset/customer.png')}></Image>
+        <Text>[강남구] 링링케이크</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -14,4 +29,15 @@ export default Stores;
 
 const styles = StyleSheet.create({
   view: {flex: 1, backgroundColor: AppStyles.color.white},
+  card: {
+    width: 166,
+    height: 250,
+    borderRadius: 8,
+    backgroundColor: 'yellow',
+    justifyContent: 'space-between',
+  },
+  image: {
+    width: 166,
+    height: 214,
+  },
 });

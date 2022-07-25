03443ead4d737cb04a590addfa4783baac45d999
vSsongv <songdh21@naver.com>
2022-07-26 00:54:11 +0900

    💄 style: SignIn page modify done

--- src/screens/auth/SignIn.tsx
+++ src/screens/auth/SignIn.tsx
@@ -180,29 +180,15 @@ const SignIn = ({navigation}: StackScreenProps<RootStackParamList>) => {
         </View>
         <View style={styles.texts}>
           <Text
-            style={[
-              styles.authText,
-              {
-                borderRightColor: AppStyles.color.IconColor,
-                borderRightWidth: 1,
-                width: 60,
-              },
-            ]}
+            style={styles.authText}
             onPress={() => navigation.navigate('SelectUserType')}>
             회원가입
           </Text>
-          <Text
-            style={[
-              styles.authText,
-              {
-                borderRightColor: AppStyles.color.IconColor,
-                borderRightWidth: 1,
-                width: 70,
-              },
-            ]}
-            onPress={() => setFindIdVisible(true)}>
+          <View style={styles.bar}></View>
+          <Text style={styles.authText} onPress={() => setFindIdVisible(true)}>
             아이디 찾기
           </Text>
+          <View style={styles.bar}></View>
           <Text style={styles.authText} onPress={() => setFindPwdVisible(true)}>
             비밀번호 찾기
           </Text>
@@ -277,10 +263,9 @@ const styles = StyleSheet.create({
   },
   texts: {
     width: 240,
-    marginTop: 31,
+    marginTop: 30,
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    justifyContent: 'space-evenly',
   },
   modal: {
     padding: AppStyles.padding.screen,
@@ -289,7 +274,7 @@ const styles = StyleSheet.create({
     height: 170,
     backgroundColor: AppStyles.color.white,
     flexDirection: 'column',
-    justifyContent: 'space-between',
+    justifyContent: 'space-evenly',
   },
   modalBtn: {
     width: 72,
@@ -312,5 +297,11 @@ const styles = StyleSheet.create({
     fontWeight: '400',
     color: AppStyles.color.black,
     opacity: 0.5,
+  },
+  bar: {
+    height: '90%',
+    borderColor: 'grey',
+    width: 1,
+    backgroundColor: AppStyles.color.IconColor,
   },
 });

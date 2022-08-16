cc550ab0ac0a044d27379d8bf04fab2580b92b3b
vSsongv <songdh21@naver.com>
2022-08-16 18:49:45 +0900

    💄 주문 상세 폰트설정 완료

--- src/screens/sellerOrder/OrderSheet.tsx
+++ src/screens/sellerOrder/OrderSheet.tsx
@@ -1,4 +1,4 @@
-import {Image, StyleSheet, Text, View, TextInput} from 'react-native';
+import {Image, StyleSheet, Text, View, TextInput, Platform} from 'react-native';
 import React, {useCallback, useRef, useState} from 'react';
 import {useMutation, useQuery, useQueryClient} from 'react-query';
 import {
@@ -162,15 +162,30 @@ const styles = StyleSheet.create({
     paddingBottom: 15.5,
   },
   title: {
-    fontWeight: '400',
     fontSize: 15,
     color: AppStyles.color.black,
+    ...Platform.select({
+      android: {
+        fontFamily: 'NotoSansKR-Medium',
+      },
+      ios: {
+        fontWeight: '400',
+      },
+    }),
   },
   text: {
-    fontWeight: '500',
     fontSize: 13,
     color: '#989898',
     lineHeight: 22,
+    ...Platform.select({
+      android: {
+        fontFamily: 'NotoSansKR-Medium',
+        lineHeight: 16,
+      },
+      ios: {
+        fontWeight: '500',
+      },
+    }),
   },
   img: {
     marginTop: 14,
@@ -191,11 +206,19 @@ const styles = StyleSheet.create({
     backgroundColor: AppStyles.color.SelectImage,
   },
   memoTitle: {
-    fontWeight: '500',
     fontSize: 12,
-    lineHeight: 14,
     color: AppStyles.color.black,
     opacity: 0.5,
+    ...Platform.select({
+      android: {
+        fontFamily: 'NotoSansKR-Medium',
+        lineHeight: 16,
+      },
+      ios: {
+        fontWeight: '500',
+        lineHeight: 14,
+      },
+    }),
   },
   memoTextInput: {
     color: AppStyles.color.black,

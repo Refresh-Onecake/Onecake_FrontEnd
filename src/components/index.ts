d715238f277d7d383886a934a31607d7c642bb5e
vSsongv <songdh21@naver.com>
2022-08-23 02:57:51 +0900

    index에 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from './seller/ToggleList';
 export * from './seller/OrderManageContent';
 export * from './seller/OrderManageFooter';
+export * from './seller/SellerMypage';
 
 export * from './consumer/Store/CakeList';
 export * from './consumer/Store/Review';
@@ -9,3 +10,4 @@ export * from './consumer/Store/StoreInfo';
 export * from './consumer/Home/CityCakes';
 export * from './consumer/Home/HotCakes';
 export * from './consumer/Home/KeywordCakes';
+export * from './consumer/ConsumerMypage';
